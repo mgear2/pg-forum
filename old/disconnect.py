ae88d7b6203d60a4d34c63fdbb9f6db760bfb981
unknown <mgear2@pdx.edu>
2019-06-01 16:07:18 -0700

    removed 'old' folder and contents

--- old/disconnect.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=import-error
-import psycopg2
-
-def disconnect(connection):
-    if connection is not None:
-        connection.close()
-        print('Database connection closed.')
-    else:
-        print('Connection was already closed!')
